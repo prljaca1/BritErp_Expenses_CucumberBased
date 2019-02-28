772356e6971fcfe63c713df29befb387d84442e7
Asel <stutdentcyber@gmail.com>
2019-02-28 17:33:08 -0500

    Asel search Func

--- src/test/java/com/cybertek/runners/SmokeTestsRunner.java
+++ src/test/java/com/cybertek/runners/SmokeTestsRunner.java
@@ -9,10 +9,10 @@ import org.junit.runner.RunWith;
                 "html:target/default-cucumber-reports",
                 "json:target/cucumber.json"
         },
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/searchFunctionality",
         glue = "com/cybertek/step_definitions"
-        , dryRun = false
-        , tags = "@wip"
+        , dryRun = true
+        , tags = "@an"
 
 )
 
